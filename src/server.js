f45c48527ee03091c7db5d8c062994de53a37a6e
Vitor Mozer <vectors3whittzz@gmail.com>
2025-10-28 19:13:17 -0300

    Making strings to read the request body and using parse.JSON

--- src/server.js
+++ src/server.js
@@ -5,26 +5,49 @@ import http from 'node:http'
 
 // Cabeçalhos (Requisição, Response) -> metadados, informações adicionais, como o front-end deve interpretar 
 const users = []
-
-const server = http.createServer((request, response) => {
+//usou await tem que transofrmar a função em assincrona(segundo plano)
+const server = http.createServer(async(request, response) => {
     const {method , url} = request
 
+    const buffers = []
+
+    for await (const chunck of request) { 
+        buffers.push(chunck)
+    }
+
+    try{
+        request.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        request.body = null    
+    }
+
     if (method == 'GET' && url == '/users'){
+
         return response
             .setHeader('Content-type','application/json')
             .end(JSON.stringify(users))
+    }
 
     if (method == 'POST' && url == '/users'){
+
+         const { name, email } = request.body
+
         users.push({
             id: 1,
-            name: 'Vitor',
-            email: 'vitor.vitor@gmail.com',
+            name,
+            email, 
         })
+
+        // users.push({
+        //     id: 1,
+        //     name: "teste",
+        //     email: "teste",
+        // })
 
         return response.writeHead(201).end()
     }
        
     return response.writeHead(404).end()
-}})
+})
 
 server.listen(3333)
